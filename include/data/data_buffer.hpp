ae8fae0fe96a9d2e4a08d59022f2be7f757769df
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2024-03-18 19:02:47 +0100

    Refactor data buffer classes

--- include/data/data_buffer.hpp
+++ include/data/data_buffer.hpp
@@ -4,18 +4,44 @@
 
 namespace IO {
 
+    /**
+     * @brief Base class for data buffers.
+     * 
+     * This class provides a base implementation for data buffers. It defines the common
+     * properties and methods that all data buffers should have.
+     */
     class DataBufferBase
     {
-
     protected:
-        std::size_t startByte;
-        std::size_t nBytes;
+        std::size_t startByte; /**< The starting byte index of the buffer. */
+        std::size_t nBytes; /**< The number of bytes in the buffer. */
 
+    public:
+        /**
+         * @brief Constructs a DataBufferBase object.
+         * 
+         * @param startByte The starting byte index of the buffer.
+         * @param nBytes The number of bytes in the buffer.
+         */
         DataBufferBase(std::size_t startByte, std::size_t nBytes)
             : startByte(startByte), nBytes(nBytes)
         {
         }
+
+        /**
+         * @brief Retrieves the buffer.
+         * 
+         * This method should be implemented by derived classes to provide access to the buffer.
+         * 
+         * @return A pointer to the buffer.
+         */
         virtual void* getBuffer() = 0;
+
+        /**
+         * @brief Destroys the DataBufferBase object.
+         * 
+         * This is a virtual destructor to ensure proper destruction of derived classes.
+         */
         virtual ~DataBufferBase() = default;
     };
 
@@ -23,30 +49,49 @@ namespace IO {
     class DataBuffer : public DataBufferBase
     {
         public:
-            std::shared_ptr<DTYPE[]> buffer;
+            std::shared_ptr<DTYPE> buffer = nullptr; /**< The buffer storing the data. */
+
+            /**
+             * @brief Constructs a DataBuffer object.
+             * 
+             * @param startByte The starting byte index of the buffer.
+             * @param nBytes The number of bytes in the buffer.
+             */
             DataBuffer(std::size_t startByte, std::size_t nBytes)
                 : DataBufferBase(startByte, nBytes)
             {
-                buffer = std::shared_ptr<DTYPE[]>(safeNew1D<DTYPE>(nBytes, __func__));;
+                buffer = std::make_shared<DTYPE[]>(nBytes);
             }
+
+            /**
+             * @brief Destroys the DataBuffer object.
+             */
             ~DataBuffer() {}
 
+            /**
+             * @brief Retrieves the buffer.
+             * 
+             * @return A pointer to the buffer.
+             */
             void* getBuffer() override {
                 return static_cast<void*>(buffer.get());
             }
 
-            //overload the [] operator
+            /**
+             * @brief Overloads the [] operator to access elements in the buffer.
+             * 
+             * @param idx The index of the element to access.
+             * @return A reference to the element at the specified index.
+             */
             DTYPE& operator[](std::size_t idx) {
                 return buffer[idx];
             }
 
+            /**
+             * @brief Clears the buffer by setting all elements to zero.
+             */
             void clearBuffer() {
                 std::memset(buffer.get(), 0, nBytes);
             }
-            
-
-            
-
     };
-
 };
