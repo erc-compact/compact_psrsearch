8c3e7cadf7ccc891945e21e1c4e3f4681f9a08dc
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2024-03-27 12:55:59 +0100

    Refactor MultiTimeSeries class to use shared pointers and add new methods for writing and flushing data

--- include/data/multi_timeseries.hpp
+++ include/data/multi_timeseries.hpp
@@ -1,57 +1,45 @@
-
+#pragma once
 #include <vector>
 #include <memory>
 #include <string>
+#include <iostream>
+#include "data/search_mode_file.hpp"
 namespace IO {
     class MultiTimeSeries {
         private:
             std::vector<float> dmList;
-            std::vector<std::unique_ptr<SearchModeFile>> outFiles;
-            std::shared_ptr<DEDISP_OUTPUT_TYPE[]> dedispersedData;
+            std::vector<std::shared_ptr<SearchModeFile>> outFiles;
+            std::shared_ptr<std::vector<DEDISP_OUTPUT_TYPE>> dedispersedData;
+            std::shared_ptr<std::vector<DEDISP_OUTPUT_TYPE>> fullDedispersedData;
+
             std::size_t totalNSamples; // this is set to all samples or the gulped samples based on whether we are writing to file or not
-            int dmListSize;
+            std::size_t dmListSize;
             std::string outDir;
             std::string outPrefix;
             std::string outSuffix;
+            bool shouldWriteToFile;
 
         public:
-            inline MultiTimeSeries(std::vector<float> dmList, std::size_t totalNSamples, std::string outDir, std::string outPrefix, std::string outSuffix, std::string outputFormat):
-                MultiTimeSeries(dmList, totalNSamples){
-                
-                this->outDir = outDir;
-                this->outPrefix = outPrefix;
-                this->outSuffix = outSuffix;
-                this->outFiles.resize(dmListSize);
-                for (std::size_t i = 0; i < dmListSize; i++){
-                    std::stringstream outFileName;
-                    outFileName << outDir << "/" << outPrefix << "_DM" << std::setprecision(3) << std::fixed << dmList[i] << "_" << outSuffix;
-                    SearchModeFile* outFile = SearchModeFile::createInstance(outFileName.str(), WRITE, outputFormat);
-                    outFiles.push_back(std::make_unique<SearchModeFile>(outFile));
-                }
-            }
-
-            inline MultiTimeSeries (std::vector<float> dmList, std::size_t nSamples){
-                this->dmList = dmList;
-                this->totalNSamples = totalNSamples;
-                this->dmListSize = dmList.size();
-                this->dedispersedData = std::make_shared<DEDISP_OUTPUT_TYPE>(nSamples * dmListSize * sizeof(DEDISP_OUTPUT_TYPE));
-            }
 
             
-            inline std::shared_ptr<DEDISP_OUTPUT_TYPE> getDataPtr() {
-                return this->dedispersedData;
-            }
+            MultiTimeSeries(std::vector<float> dmList, std::string outDir, std::string outPrefix, std::string outSuffix, std::string outputFormat);
+            MultiTimeSeries(std::vector<float> dmList, std::size_t gulpNSamples, std::size_t totalNSamples);
+            std::shared_ptr<std::vector<DEDISP_OUTPUT_TYPE>> getCurrentDedispersedDataPtr();
+            void flush();
+            virtual ~MultiTimeSeries() = default;
 
-            DEDISP_OUTPUT_TYPE& operator()(std::size_t iDm, std::size_t iSample){
-                return this->dedispersedData.get()[iDm * this->dmListSize + iSample];
-            }
 
-            inline void writeToFile(){
-                for (std::size_t i = 0; i < dmListSize; i++){
-                    outFiles[i]->writeNBytes(this->dedispersedData.get()[i], this->totalNSamples * sizeof(DEDISP_OUTPUT_TYPE));
-                }
-            }
-            ~MultiTimeSeries();
+        private:
+            void writeToFile();
+
+            // DEDISP_OUTPUT_TYPE& operator()(std::size_t iDm, std::size_t iSample){
+            //     return this->dedispersedData.get()[iDm * this->dmListSize + iSample];
+            // }
+            // inline void writeToFile(){
+            //     for (std::size_t i = 0; i < dmListSize; i++){
+            //         outFiles[i]->writeNBytes(this->dedispersedData.get()[i], this->totalNSamples * sizeof(DEDISP_OUTPUT_TYPE));
+            //     }
+            // }
 
     };
 };
---
+++ src/data/multi_timeseries.cpp
@@ -0,0 +1,37 @@
+#include "data/multi_timeseries.hpp"
+
+using namespace IO;
+
+MultiTimeSeries::MultiTimeSeries(std::vector<float> dmList, std::string outDir, std::string outPrefix, std::string outSuffix, std::string outputFormat)
+{
+    this->outDir = outDir;
+    this->outPrefix = outPrefix;
+    this->outSuffix = outSuffix;
+    this->outFiles.resize(dmListSize);
+    for (std::size_t i = 0; i < dmListSize; i++){
+        std::stringstream outFileName;
+        outFileName << outDir << "/" << outPrefix << "_DM" << std::setprecision(3) << std::fixed << dmList[i] << "_" << outSuffix;
+        outFiles.push_back(SearchModeFile::createInstance(outFileName.str(), WRITE, outputFormat));
+    }
+}
+
+MultiTimeSeries::MultiTimeSeries(std::vector<float> dmList, std::size_t gulpNSamples, std::size_t totalNSamples){
+    this->dmList = dmList;
+    this->dmListSize = dmList.size();
+    this->dedispersedData = std::make_shared<std::vector<DEDISP_OUTPUT_TYPE>>(gulpNSamples * dmListSize);
+    this->fullDedispersedData = std::make_shared<std::vector<DEDISP_OUTPUT_TYPE>>(totalNSamples * dmListSize);
+}
+
+std::shared_ptr<std::vector<DEDISP_OUTPUT_TYPE>> MultiTimeSeries::getCurrentDedispersedDataPtr() {
+    return this->dedispersedData;
+}
+
+void MultiTimeSeries::flush(){
+    if (this->shouldWriteToFile){
+        this->writeToFile();
+    }
+    else {
+        fullDedispersedData->insert(fullDedispersedData->end(), std::make_move_iterator(dedispersedData->begin()), std::make_move_iterator(dedispersedData->end()));
+        dedispersedData->clear();
+    }
+}
